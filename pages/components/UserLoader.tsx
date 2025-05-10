1c79dbb4f3951e17064202bc402ec8d3e9dc5caf
hasaanmumtaz6 <hasaanmumtaz6@gmail.com>
2025-05-11 00:04:11 +0500

    Design of writer and profile abr side

---
+++ pages/components/UserLoader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserLoader = () => {
+  return (
+    <div className="loader-box">
+      <span className="user-loader"></span>
+      <span className="loader">Load&nbsp;ng</span>
+    </div>
+  );
+};
+
+export default UserLoader;
